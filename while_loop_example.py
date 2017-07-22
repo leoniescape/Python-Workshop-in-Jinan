89f7a808de9d4fd60d2f60683fb76dab2e7ed358
Hao Meiru <leonie.hao@gmail.com>
2017-07-22 15:20:04 +0200

    Update while_loop_example.py

--- while_loop_example.py
+++ while_loop_example.py
@@ -1,9 +1,9 @@
 #calculate the number of students in this classroom
 
 total = 0
-item  = int( input("Enter value: ") )
-while item != -1:
-    total += item
-    item  = int( input("Enter value: ") )
+student  = int( input("Enter value: ") )
+while student != -1:
+    total += student
+    student  = int( input("Enter value: ") )
 print( "total=" , total )
 
