ef09b56c281e86479d583358eb33bcc1e1e30fcb
Hao Meiru <leonie.hao@gmail.com>
2017-07-22 14:36:33 +0200

    Add files via upload
    请大家run之前先试着想一下结果~

---
+++ if_exercise.py
@@ -0,0 +1,23 @@
+condition_A = True
+condition_B = True
+condition_C = True
+condition_D = True
+condition_E = True
+condition_F = True
+
+if condition_A:
+   if condition_B:
+      print( "1" )
+   else:
+      print( "2" )
+   if condition_C:
+      print( "3" )
+   elif condition_D:
+      print( "4" )
+      if condition_E:
+         print( "5" )
+      else:
+         print( "6" )
+   else:
+      print( "7" )
+   print( "8" )
